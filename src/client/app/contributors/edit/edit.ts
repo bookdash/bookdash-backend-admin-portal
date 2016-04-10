ab04b82a6782e33a2d8eeabe12c7e83e87404546
Lance Gleason <lgleasain@yahoo.com>
2016-04-10 02:02:37 -0400

    adding in some of contributor editing and avatar display

---
+++ src/client/app/contributors/edit/edit.ts
@@ -0,0 +1,45 @@
+import {Component, Input, Inject} from 'angular2/core';
+import {MdButton} from '@angular2-material/button';
+import {FirebaseRef} from 'angularfire2';
+declare var CONTRIBUTORS: any;
+
+@Component({
+  selector: '[contributor]',
+  templateUrl: 'app/contributors/edit/edit.html',
+  styleUrls: ['app/contributors/edit/edit.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Edit {
+    @Input() key: string; 
+    @Input() name: string;
+    @Input() avatar: string
+    edit: boolean = false;
+    @Input() contributor: any; 
+
+    constructor(@Inject(FirebaseRef) public firebase:Firebase) {}
+
+    doEdit(){
+        /*this.firebase.child(CONTRIBUTORS)
+            .child(this.key)
+            .once("value",
+                  (contributor) => {
+                      this.contributor = contributor.val();
+                      this.edit = true;
+                  }
+                 );
+        */
+        this.edit = true
+    }
+
+    doSave(){
+        var key: string = this.contributor.$key
+        delete this.contributor.$key
+        this.firebase.child(CONTRIBUTORS).child(key).set(
+            this.contributor
+            );
+        this.edit = false;
+    }
+
+}
