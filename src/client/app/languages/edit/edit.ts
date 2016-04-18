1746e0b25b948b29d921a7a4e6907cc25d16f146
Lance Gleason <lgleasain@yahoo.com>
2016-04-17 20:22:13 -0400

    finished up language crud

--- src/client/app/languages/edit/edit.ts
+++ src/client/app/languages/edit/edit.ts
@@ -1,24 +1,34 @@
 import {Component, Input, Inject} from 'angular2/core';
 import {MdButton} from '@angular2-material/button';
 import {FirebaseRef} from 'angularfire2';
+import {MdCheckbox} from '@angular2-material/checkbox';
 declare var LANGUAGES: any;
 
 @Component({
-    selector: 'edit',
+    selector: '[language]',
     templateUrl: 'app/languages/edit/edit.html',
     styleUrls: ['app/languages/edit/edit.css'],
     providers: [],
-    directives: [],
+    directives: [MdButton, MdCheckbox],
     pipes: []
 })
 export class Edit {
-    @Input() key: string; 
-    @Input() languageName: string;
-    @Input() languageAbbreviation: string;
-    @Input() enabled: string;
+    @Input() language: any;
     edit: boolean = false;
-    language: any;
 
     constructor(@Inject(FirebaseRef) public firebase:Firebase) {}
+
+    doEdit(){
+        this.edit = true
+    }
+    
+    doSave(){
+        var key: string = this.language.$key
+        delete this.language.$key
+        this.firebase.child(LANGUAGES).child(key).set(
+            this.language
+            );
+        this.edit = false;
+    }
 
 }
