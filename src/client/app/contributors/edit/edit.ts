a48b0952966ebea93d37ecdc2c75e607178defe3
Lance Gleason <lgleasain@yahoo.com>
2016-04-26 11:04:22 -0400

    finished contributor editing functionality

--- src/client/app/contributors/edit/edit.ts
+++ src/client/app/contributors/edit/edit.ts
@@ -1,53 +1,68 @@
 import {Component, Input, Inject} from 'angular2/core';
 import {MdButton} from '@angular2-material/button';
 import {FirebaseRef} from 'angularfire2';
+import {MdCheckbox} from '@angular2-material/checkbox';
 declare var ROLES: any;
 declare var CONTRIBUTORS: any;
 
 @Component({
-  selector: '[contributor]',
-  templateUrl: 'app/contributors/edit/edit.html',
-  styleUrls: ['app/contributors/edit/edit.css'],
-  providers: [],
-  directives: [],
-  pipes: []
+    selector: '[contributor]',
+    templateUrl: 'app/contributors/edit/edit.html',
+    styleUrls: ['app/contributors/edit/edit.css'],
+    providers: [],
+    directives: [MdCheckbox],
+    pipes: []
 })
 export class Edit {
     edit: boolean = false;
     @Input() contributor: any; 
     roles: any;
     roleCheckboxes: any = [];
+    rolesString: string = "";
 
     constructor(@Inject(FirebaseRef) public firebase:Firebase) {
-    }
-
-    doEdit(){
-
         this.firebase.child(ROLES)
             .once("value",
                   (roles) => {
                       this.roles = roles.val();
-                      this.edit = true;
 
-                      console.log(this.contributor.roles)
-                      console.log(this.roles.keys)
-                      for (var role in this.roles) {
-                          console.log(role)
-                          console.log(this.roles[role])
-                          /*this.roleCheckboxes.push({key: role.key(),
-                                               name: role.val().name,
-                                               isChecked: (role.key() in this.contributor.roles)})*/
+                      this.roleCheckboxes = []
+                      for (var roleKey in this.roles) {
+                          var role = this.roles[roleKey]
+                          if(this.contributor.roles !== undefined){
+                              if(this.contributor.roles[roleKey] == true){
+                                  this.rolesString = this.rolesString + " " +
+                                      role.name;
+                              }
+                              this.roleCheckboxes.push({key: roleKey,
+                                                        name: role.name,
+                                                        //isChecked: false})
+                                                        isChecked: (this.contributor.roles[roleKey] == true)})
+                          }
                       }
                   }
                  );
+
+    }
+
+    doEdit(){
+        this.edit = true
     }
 
     doSave(){
         var key: string = this.contributor.$key
         delete this.contributor.$key
+
+        this.contributor.roles = {}
+        for(var roleCheckbox in this.roleCheckboxes){
+            if(this.roleCheckboxes[roleCheckbox].isChecked){
+                this.contributor.roles[this.roleCheckboxes[roleCheckbox].key] = true
+            }
+        }
+
         this.firebase.child(CONTRIBUTORS).child(key).set(
             this.contributor
-            );
+        );
         this.edit = false;
     }
 
