d049970fcff629fcd1575b3c11023ff5ae49a8d1
Rebecca Franks <franks.rebecca@gmail.com>
2016-08-12 13:57:56 +0200

    Started work on adding a createdDate field to book.

--- src/app/list-books/add/add.ts
+++ src/app/list-books/add/add.ts
@@ -1,10 +1,10 @@
 import {Component, Input, Inject, Output, EventEmitter} from '@angular/core';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {MdButton} from '@angular2-material/button';
 import {FirebaseRef, AngularFire} from 'angularfire2';
 import {MdCheckbox} from '@angular2-material/checkbox';
 import {MdInput} from '@angular2-material/input';
 import {Observable} from 'rxjs/Observable';
-import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 
 declare var ROLES: any;
 declare var CONTRIBUTORS: any;
@@ -16,7 +16,7 @@ declare var BOOKS: any;
     selector: 'add',
     templateUrl: 'app/list-books/add/add.html',
     styleUrls: ['app/list-books/add/add.css'],
-    providers: [],
+    providers: [AngularFire],
     directives: [MD_CARD_DIRECTIVES, MdButton, MdCheckbox, MdInput],
     pipes: []
 })
@@ -28,6 +28,7 @@ export class Add {
     bookTitle: string;
     bookDescription: string;
     bookCoverPageUrl: string;
+    bookCreatedDate: any = new Date().getTime();
     bookUrl: string;
     bookEnabled: any;
     bookLanguage: any;
@@ -62,6 +63,22 @@ export class Add {
         )
     }
 
+    get displayDate() {
+        let s = new Date(this.bookCreatedDate).toISOString().substring(0, 10);
+        console.log("display date called:" + s);
+        return s;
+    }
+
+    set displayDate(e: any){
+        console.log("displayDate:" + e);
+        let dateSplit = e.split('-');
+        let d = new Date(Date.UTC(Number(dateSplit[0]), Number(dateSplit[1]) - 1, Number(dateSplit[2])));
+        //let d = new Date(e);
+        d.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate() + 1);
+        this.bookCreatedDate = d.getTime();
+        console.log("displayDate: end:" + this.bookCreatedDate);
+    }
+
     doSave() {
         var contributorsHash = {}
 
@@ -71,6 +88,8 @@ export class Add {
                 contributorsHash[this.contributorCheckboxes[contributorCheckboxIndex].key] = true
             }
         }
+      //  var date = new Date(this.bookCreatedDate);
+        console.log(this.bookCreatedDate);
         var bookData = {
             bookTitle: this.bookTitle,
             bookDescription : this.bookDescription,
@@ -78,6 +97,7 @@ export class Add {
             bookUrl: this.bookUrl,
             bookLanguage: this.bookLanguage,
             bookEnabled: this.bookEnabled.checked,
+            createdDate: this.bookCreatedDate,
             contributors: contributorsHash
         }
 
--- src/app/list-books/edit/edit.ts
+++ src/app/list-books/edit/edit.ts
@@ -31,7 +31,22 @@ export class Edit {
 
     constructor(public angularFire: AngularFire) {
     }
-    
+
+    get displayDate(){
+        if (this.book.createdDate == null){
+            this.book.createdDate = new Date().getTime();
+        }
+        return new Date(this.book.createdDate).toISOString().substring(0, 10);
+    }
+
+    set displayDate(e){
+        console.log("displayDate:" + e);
+        let dateSplit = e.split('-');
+        let d = new Date(Date.UTC(Number(dateSplit[0]), Number(dateSplit[1]) - 1, Number(dateSplit[2])));
+        //let d = new Date(e);
+        d.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate() + 1);
+        this.book.createdDate = d.getTime();
+    }
     ngOnInit(){
         this.key = this.book.$key
         if(this.book != 'book')
@@ -71,6 +86,7 @@ export class Edit {
                     
                 }
             )
+            
 
         }
     }
